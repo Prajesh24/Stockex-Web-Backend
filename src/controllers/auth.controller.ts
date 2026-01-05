f97ca8c46e2fe02ce1d9aec5c68b9655ef0a53a0
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2026-01-05 17:48:40 +0545

    adding controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ let userService = new UserService();
 export class AuthController {
     async register(req: Request, res: Response) {
         try {
-            const parsedData = CreateUserDTO.safeParse(req.body); // validate request body
+            const parsedData = CreateUserDTO.safeParse(req.body); 
             if (!parsedData.success) { // validation failed
                 return res.status(400).json(
                     { success: false, message: z.prettifyError(parsedData.error) }
